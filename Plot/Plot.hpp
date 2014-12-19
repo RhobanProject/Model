1d09eb0bae5292dd7a6f97903b800197901882c1
Leph <leph.quentinrouxel@gmail.com>
2014-12-19 22:43:05 +0100

    Ajout de la lib Plot

---
+++ Plot/Plot.hpp
@@ -0,0 +1,377 @@
+#ifndef LEPH_PLOT_HPP
+#define LEPH_PLOT_HPP
+
+#include <vector>
+#include <stdexcept>
+#include <sstream>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "Types/VectorLabel.hpp"
+
+namespace Leph {
+
+/**
+ * Plot
+ *
+ * Programming interface with
+ * the Linux ploting utility 
+ * Gnuplot using VectorLabel
+ */
+class Plot
+{
+    public:
+
+        /**
+         * Initialization
+         */
+        Plot() :
+            _rangeMinX(1.0),
+            _rangeMaxX(0.0),
+            _rangeMinY(1.0),
+            _rangeMaxY(0.0),
+            _rangeMinZ(1.0),
+            _rangeMaxZ(0.0),
+            _pipeFd(-1)
+        {
+        }
+
+        /**
+         * Add a (vector) point
+         */
+        inline void add(const VectorLabel& vect)
+        {
+            _database.push_back(vect);
+        }
+
+        /**
+         * Request a 2D plot with X and Y axis
+         * X axis could be "index"
+         * Y axis could be "all"
+         */
+        inline Plot& plot(const std::string& xAxis, 
+            const std::string& yAxis)
+        {
+            for (size_t i=0;i<_plots2D.size();i++) {
+                if (_plots2D[i].xAxis == xAxis && _plots2D[i].yAxis == yAxis) {
+                    return *this;
+                }
+            }
+
+            if (yAxis == "all") {
+                for (const auto& label : _database.back().labels()) {
+                    if (label.first != xAxis) {
+                        plot(xAxis, label.first);
+                    }
+                }
+                return *this;
+            }
+
+            Plot2D request;
+            request.xAxis = xAxis;
+            request.yAxis = yAxis;
+            _plots2D.push_back(request);
+
+            return *this;
+        }
+        
+        /**
+         * Request a 3D plot with X, Y and Z axis 
+         * X axis could be "index"
+         * Y axis could be "index"
+         */
+        inline Plot& plot(const std::string& xAxis, 
+            const std::string& yAxis, const std::string& zAxis)
+        {
+            for (size_t i=0;i<_plots2D.size();i++) {
+                if (_plots3D[i].xAxis == xAxis && 
+                    _plots3D[i].yAxis == yAxis &&
+                    _plots3D[i].zAxis == zAxis) {
+                    return *this;
+                }
+            }
+
+            Plot3D request;
+            request.xAxis = xAxis;
+            request.yAxis = yAxis;
+            request.zAxis = zAxis;
+            _plots3D.push_back(request);
+            
+            return *this;
+        }
+
+        /**
+         * Set x,y,z ploting range
+         */
+        inline Plot& rangeX(double min, double max)
+        {
+            _rangeMinX = min;
+            _rangeMaxX = max;
+
+            return *this;
+        }
+        inline Plot& rangeY(double min, double max)
+        {
+            _rangeMinY = min;
+            _rangeMaxY = max;
+
+            return *this;
+        }
+        inline Plot& rangeZ(double min, double max)
+        {
+            _rangeMinZ = min;
+            _rangeMaxZ = max;
+
+            return *this;
+        }
+
+        /**
+         * Render the plot
+         */
+        inline void render()
+        {
+            if (_plots2D.size() > 0 && _plots3D.size() > 0) {
+                throw std::logic_error("Plot error 2d and 3d request");
+            }
+            if (_plots2D.size() == 0 && _plots3D.size() == 0) {
+                return;
+            }
+
+            if (_plots2D.size() > 0) {
+            }
+            if (_plots3D.size() > 0) {
+            }
+            
+            closeGnuplotInstance();
+            createGnuplotInstance();
+            doPlotting();
+            closeGnuplotInstance();
+            clear();
+        }
+
+        /**
+         * Reset points database, plot
+         * requests and both
+         */
+        inline void clearPoints()
+        {
+            _database.clear();
+        }
+        inline void clearPlots()
+        {
+            _plots2D.clear();
+            _plots3D.clear();
+        }
+        inline void clear()
+        {
+            clearPoints();
+            clearPlots();
+        }
+
+    private:
+
+        /**
+         * Plot request 2D and 3D
+         * structures
+         */
+        struct Plot2D {
+            std::string xAxis;
+            std::string yAxis;
+        };
+        struct Plot3D {
+            std::string xAxis;
+            std::string yAxis;
+            std::string zAxis;
+        };
+
+        /**
+         * Vector labeled points database
+         */
+        std::vector<VectorLabel> _database;
+
+        /**
+         * Plot request container
+         */
+        std::vector<Plot2D> _plots2D;
+        std::vector<Plot3D> _plots3D;
+
+        /**
+         * Plot x,y,z min,max range
+         * If max > min, auto scaling is used
+         */
+        double _rangeMinX;
+        double _rangeMaxX;
+        double _rangeMinY;
+        double _rangeMaxY;
+        double _rangeMinZ;
+        double _rangeMaxZ;
+        
+        /**
+         * Gnuplot pipe file descriptor
+         */
+        int _pipeFd;
+        
+        /**
+         * Fork current process to create a new GnuPlot window
+         */
+        inline void createGnuplotInstance()
+        {
+            //Creating communication pipe
+            int pipefd[2];
+            if (pipe(pipefd) == -1) {
+                throw std::runtime_error("Plot failed to create pipe");
+            }
+            
+            //Forkink current process
+            pid_t pid = fork();
+            if (pid > 0) {
+                //Closing reading pipe end
+                close(pipefd[0]);
+                //Saving pipe fd
+                _pipeFd = pipefd[1];
+            } else if (pid == 0) {
+                //Closing writting pipe end
+                close(pipefd[1]);
+                //Redirecting reading pipe end to standart input
+                if (dup2(pipefd[0], STDIN_FILENO) == -1) {
+                    throw std::runtime_error("Plot failed to dup2");
+                }
+                //Closing output and err
+                int null = open("/dev/null", O_WRONLY);
+                if (dup2(null, STDOUT_FILENO) == -1) {
+                    throw std::runtime_error("Plot failed to dup2");
+                }
+                if (dup2(null, STDERR_FILENO) == -1) {
+                    throw std::runtime_error("Plot failed to dup2");
+                }
+                //Calling Gnuplot
+                execlp("gnuplot", "gnuplot", "-p", "-", NULL);
+            } else {
+                throw std::runtime_error("Plot failed to fork");
+            }
+        }
+
+        /**
+         * Close the openned pipe to GnuPlot window instance
+         * (Gnuplot window is not close)
+         */
+        inline void closeGnuplotInstance()
+        {
+            if (_pipeFd != -1) {
+                close(_pipeFd);
+                _pipeFd = -1;
+            }
+        }
+
+        /**
+         * Send data and plot commands to GnuPlot instance
+         */
+        inline void doPlotting()
+        {
+            if (_pipeFd <= 0) {
+                throw std::logic_error("Plot closed pipe fd");
+            }
+            
+            std::string commands;
+            std::string data;
+
+            commands += "set grid;";
+            if (_rangeMinX < _rangeMaxX) {
+                std::ostringstream oss;
+                oss << "set xrange[" << _rangeMinX;
+                oss << ":" << _rangeMaxX << "];";
+                commands += oss.str();
+            }
+            if (_rangeMinY < _rangeMaxY) {
+                std::ostringstream oss;
+                oss << "set yrange[" << _rangeMinY;
+                oss << ":" << _rangeMaxY << "];";
+                commands += oss.str();
+            }
+            if (_rangeMinZ < _rangeMaxZ) {
+                std::ostringstream oss;
+                oss << "set zrange[" << _rangeMinZ;
+                oss << ":" << _rangeMaxZ << "];";
+                commands += oss.str();
+            }
+
+            //Create commands and data to send to gnuplot
+            if (_plots2D.size() != 0) {
+                commands += "plot ";
+            } else {
+                commands += "splot ";
+            }
+            
+            bool isFirst = true;
+            for (size_t i=0;i<_plots2D.size();i++) {
+                if (!isFirst) {
+                    commands += ", ";
+                }
+                isFirst = false;
+                commands += "'-' using 1:2 with linespoints";
+                commands += " title '" + _plots2D[i].xAxis + " --> " + _plots2D[i].yAxis + "' ";
+                for (size_t j=0;j<_database.size();j++) {
+                    if (
+                        (_plots2D[i].xAxis != "index" &&
+                        !_database[j].exist(_plots2D[i].xAxis)) || 
+                        !_database[j].exist(_plots2D[i].yAxis)
+                    ) {
+                        continue;
+                    }
+                    std::ostringstream oss;
+                    if (_plots2D[i].xAxis == "index") {
+                        oss << j << " " << _database[j](_plots2D[i].yAxis);
+                    } else {
+                        oss << _database[j](_plots2D[i].xAxis) << " " 
+                            << _database[j](_plots2D[i].yAxis);
+                    }
+                    data += oss.str() + "\n";
+                }
+                data += "end\n";
+            }
+            for (size_t i=0;i<_plots3D.size();i++) {
+                if (!isFirst) {
+                    commands += ", ";
+                }
+                isFirst = false;
+                commands += "'-' using 1:2:3 with points";
+                commands += " title '" + _plots3D[i].xAxis + "," + _plots3D[i].yAxis + " --> " + _plots3D[i].zAxis + "' ";
+                for (size_t j=0;j<_database.size();j++) {
+                    if (
+                        (_plots3D[i].xAxis != "index" &&
+                        !_database[j].exist(_plots3D[i].xAxis)) || 
+                        (_plots3D[i].yAxis != "index" &&
+                        !_database[j].exist(_plots3D[i].yAxis)) || 
+                        !_database[j].exist(_plots3D[i].zAxis)
+                    ) {
+                        continue;
+                    }
+                    std::ostringstream oss;
+                    if (_plots3D[i].xAxis == "index") {
+                        oss << j << " ";
+                    } else {
+                        oss << _database[j](_plots3D[i].xAxis) << " ";
+                    }
+                    if (_plots3D[i].yAxis == "index") {
+                        oss << j << " ";
+                    } else {
+                        oss << _database[j](_plots3D[i].yAxis) << " ";
+                    }
+                    oss << _database[j](_plots3D[i].zAxis);
+                    data += oss.str() + "\n";
+                }
+                data += "end\n";
+            }
+            commands += ";\n";
+
+            //Sending commands and data to GnuPlot
+            write(_pipeFd, commands.c_str(), commands.length());
+            write(_pipeFd, data.c_str(), data.length());
+        }
+};
+
+}
+
+#endif
+
---
+++ Tests/testPlot.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Types/VectorLabel.hpp"
+#include "Plot/Plot.hpp"
+
+int main()
+{
+    Leph::Plot plot;
+    for (int i=0;i<100;i++) {
+        Leph::VectorLabel vect({
+            std::make_pair("toto", i*i),
+            std::make_pair("titi", -i),
+            std::make_pair("tata", 1.0),
+            std::make_pair("tete", -0.5*i)
+        });
+        plot.add(vect);
+    }
+    plot.rangeX(0.0, 10.0).rangeY(-2.0, 2.0)
+        .plot("index", "all").plot("index", "titi").render();
+
+    return 0;
+}
+
