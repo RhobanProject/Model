05645a38fddbe1c64211e59a32da56a9b32d98d6
Leph <leph.quentinrouxel@gmail.com>
2015-01-09 17:42:43 +0100

    Experiment code de base

--- Experiments/CartWalkGradientOptim/main.cpp
+++ Experiments/CartWalkGradientOptim/main.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
+#include <cmath>
+#include <chrono>
+#include <thread>
 
 //SDK
 #include <main/Command.h>
-#include <rhoban/robot/Robots.h>
+#include <robot/Robot.h>
 
-int main
+int main()
 {
     try {
-        Rhoban::Robots robots;
-	robots.loadYaml("config.yml");
-        Rhoban::Robot* robot = robots["local"];
-        robot->loadMove("/home/rhoban/Environments/RhobanServer/Mowgly/Moves/CartWalk.move.xml", true);
-        robot->startMove("CartWalk", 0, 2000);
+        Rhoban::Robot robot(new CommandsStore, "local");
+        robot.connect("127.0.0.1", 7777);
+        robot.loadEnvironment("/home/rhoban/Environments/RobotBoard/Mowgly/");
+        Rhoban::Motors* motors = robot.getMotors();
+
+        std::this_thread::sleep_for(std::chrono::seconds(4));
+        motors->get("Coude G")->setRelAngle(0.0);
+        std::this_thread::sleep_for(std::chrono::seconds(4));
+
+        for (double t=0.0;t<=5.0;t+=0.02) {
+            motors->get("Coude G")->setRelAngle(5.0*sin(2.0*M_PI*t));
+            std::this_thread::sleep_for(std::chrono::milliseconds(20));
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(4));
+        motors->get("Coude G")->setRelAngle(0.0);
     } catch (std::string err) {
         std::cout << "Exception : " << err << std::endl;
     }
