129b514340d686005ad3e998d353d25577b8575b
Leph <leph.quentinrouxel@gmail.com>
2015-01-12 18:54:24 +0100

    Supression de la gestion des bras dans CartWalk et modif paramètres hipOffset

--- CartWalk/CartWalkProxy.cpp
+++ CartWalk/CartWalkProxy.cpp
@@ -28,14 +28,12 @@ VectorLabel CartWalkProxy::buildOutputs() const
         "left knee", 0.0,
         "left foot pitch", 0.0,
         "left foot roll", 0.0,
-        "left arm", 0.0,
         "right hip pitch", 0.0,
         "right hip roll", 0.0,
         "right hip yaw", 0.0,
         "right knee", 0.0,
         "right foot pitch", 0.0,
-        "right foot roll", 0.0,
-        "right arm", 0.0
+        "right foot roll", 0.0
     );
 }
 VectorLabel CartWalkProxy::buildInfo() const
@@ -58,15 +56,14 @@ VectorLabel CartWalkProxy::buildStaticParams() const
         "swingGain", 0.4,
         "swingPhase", 0.0,
         "swingHeight", 0.0,
-        "armsGain", 0.0,
         "xOffset", -0.7,
         "yOffset", 1.0,
         "zOffset", 4.0,
+        "hipOffset", 17.0,
         "yLat", 0.0,
         "swingForce", 0.0,
         "riseRatio", 0.5,
-        "riseStepPhase", 0.0,
-        "hipOffset", -17
+        "riseStepPhase", 0.0
     );
 }
 VectorLabel CartWalkProxy::buildDynamicParams() const
@@ -87,15 +84,14 @@ VectorLabel CartWalkProxy::buildStaticParamsMin() const
         "swingGain", 0.0,
         "swingPhase", 0.0,
         "swingHeight", 0.0,
-        "armsGain", -20.0,
         "xOffset", -0.7,
         "yOffset", 1.0,
         "zOffset", 4.0,
+        "hipOffset", 0.0,
         "yLat", 0.0,
         "swingForce", 0.0,
         "riseRatio", 0.5,
-        "riseStepPhase", 0.0,
-        "hipOffset", 0.0
+        "riseStepPhase", 0.0
     );
 }
 VectorLabel CartWalkProxy::buildStaticParamsMax() const
@@ -106,15 +102,14 @@ VectorLabel CartWalkProxy::buildStaticParamsMax() const
         "swingGain", 2.0,
         "swingPhase", 7.0,
         "swingHeight", 0.0,
-        "armsGain", 20.0,
         "xOffset", -0.7,
         "yOffset", 1.0,
         "zOffset", 4.0,
+        "hipOffset", 0.0,
         "yLat", 0.0,
         "swingForce", 0.0,
         "riseRatio", 0.5,
-        "riseStepPhase", 0.0,
-        "hipOffset", 0.0
+        "riseStepPhase", 0.0
     );
 }
 VectorLabel CartWalkProxy::buildDynamicParamsMin() const
@@ -146,10 +141,10 @@ VectorLabel CartWalkProxy::exec(
     _walk.swingGain = staticParams("swingGain");
     _walk.swingPhase = staticParams("swingPhase");
     _walk.swingHeight = staticParams("swingHeight");
-    _walk.armsGain = staticParams("armsGain");
     _walk.xOffset = staticParams("xOffset");
     _walk.yOffset = staticParams("yOffset");
     _walk.zOffset = staticParams("zOffset");
+    _walk.hipOffset = staticParams("hipOffset");
     _walk.yLat = staticParams("yLat");
     _walk.swingForce = staticParams("swingForce");
     _walk.riseRatio = staticParams("riseRatio");
@@ -169,18 +164,13 @@ VectorLabel CartWalkProxy::exec(
     outputs("left knee") = _walk.a_l_knee;
     outputs("left foot pitch") = _walk.a_l_foot_pitch;
     outputs("left foot roll") = _walk.a_l_foot_roll;
-    outputs("left arm") = _walk.a_l_arm;
     outputs("right hip pitch") = _walk.a_r_hip_pitch;
     outputs("right hip roll") = _walk.a_r_hip_roll;
     outputs("right hip yaw") = _walk.a_r_hip_yaw;
     outputs("right knee") = _walk.a_r_knee;
     outputs("right foot pitch") = _walk.a_r_foot_pitch;
     outputs("right foot roll") = _walk.a_r_foot_roll;
-    outputs("right arm") = _walk.a_r_arm;
 
-    outputs("left hip pitch") += staticParams("hipOffset");
-    outputs("right hip pitch") += staticParams("hipOffset");
-    
     VectorLabel info = buildInfo();
     info("phase") = _walk.t;
     info("left spline X") = _walk.sLX;
