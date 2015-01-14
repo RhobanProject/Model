3a5d9ec2c6302efd5ed892aaf4b32881eb575232
Leph <leph.quentinrouxel@gmail.com>
2015-01-14 16:44:22 +0100

    Mise à jour et refactoring CartWalk Gradient Experiment

--- Experiments/CartWalkGradientOptim/main.cpp
+++ Experiments/CartWalkGradientOptim/main.cpp
@@ -2,8 +2,6 @@
 #include <cmath>
 #include <chrono>
 #include <thread>
-#include <signal.h>
-#include <unistd.h>
 
 //CODE
 #include "Types/VectorLabel.hpp"
@@ -12,110 +10,87 @@
 //UTILS
 #include "MotionCapture.hpp"
 #include "SDKConnection.hpp"
+#include "SDKInterface.hpp"
 
-void runWalk(Leph::SDKConnection& sdkConnection, Leph::CartWalkProxy& walk, 
-    const Leph::VectorLabel& staticParams, const Leph::VectorLabel& dynamicParams, 
-    double duration)
+int main()
 {
+    Leph::SDKConnection sdkConnection;
+    
+    Leph::CartWalkProxy walk;
+    Leph::VectorLabel staticParams = walk.buildStaticParams();
+    Leph::VectorLabel dynamicParams = walk.buildDynamicParams();
+    dynamicParams("step") = 12.0;
+    
+    Rhoban::MotionCapture motionCapture;
+    motionCapture.setCaptureStream("tcp://192.168.16.10:3232");
+    motionCapture.averageCoefPos = 0.7;
+    motionCapture.maxInvalidTick = 20;
+
+    Leph::VectorLabel posReferences(
+        "forward", -0.336085,
+        "lateral", -0.0763521,
+        "turn", 0.0);
+    Leph::VectorLabel servoGains(
+        "forward", 20.0,
+        "lateral", 125.0,
+        "turn", 1.25);
+    Leph::VectorLabel deltas(
+        "forward", 0.0,
+        "lateral", 0.0,
+        "turn", 0.0);
+    Leph::VectorLabel discountCoef(
+        "coef", 0.8);
+    Leph::VectorLabel mocapPosError(
+        "forward", 0.0,
+        "lateral", 0.0,
+        "turn", 0.0);
+    Leph::VectorLabel mocapIsValid(
+        "isValid", 0);
+    
+    std::string statusEnabled = "Walk is Disabled";
+    Leph::SDKInterface interface(sdkConnection, "CartWalk Gradient Optim");
+    interface.addParameters("Dynamic walk parameters", dynamicParams);
+    interface.addParameters("Static walk parameters", staticParams);
+    interface.addParameters("Servo gain", servoGains);
+    interface.addParameters("Reference positions", posReferences);
+    interface.addMonitors("Motion Capture isValid", mocapIsValid);
+    interface.addMonitors("Motion Capture position error", mocapPosError);
+    interface.addMonitors("Deltas", deltas);
+    interface.addStatus(statusEnabled);
+    interface.addBinding(' ', "Toggle walk enable", [&dynamicParams, &statusEnabled](){
+        dynamicParams("enabled") = !dynamicParams("enabled");
+        if (dynamicParams("enabled")) {
+            statusEnabled = "Walk is Enabled";
+        } else {
+            statusEnabled = "Walk is Disabled";
+        }
+    });
+    
     const double freq = 50.0;
-    for (double t=0.0;t<=duration;t+=1.0/freq) {
-        walk.exec(1.0/freq, dynamicParams, staticParams);
+    while (interface.tick()) {
+        motionCapture.tick(1.0/freq);
+        mocapIsValid("isValid") = motionCapture.pos.isValid;
+        mocapPosError("forward") = motionCapture.pos.z - posReferences("forward");
+        mocapPosError("lateral") = motionCapture.pos.x - posReferences("lateral");
+        mocapPosError("turn") = motionCapture.pos.azimuth - posReferences("turn");
+
+        if (mocapIsValid("isValid")) {
+            Leph::Vector tmp1 = discountCoef("coef")*deltas.vect();
+            Leph::Vector tmp2 = (1.0-discountCoef("coef"))
+                *(servoGains.vect().array() * mocapPosError.vect().array());
+            deltas.vect() = tmp1 + tmp2;
+        }
+
+        Leph::VectorLabel tmpDynamicParams = dynamicParams;
+        tmpDynamicParams("step") += deltas("forward");
+        tmpDynamicParams("lateral") += deltas("lateral");
+        tmpDynamicParams("turn") += deltas("turn");
+
+        walk.exec(1.0/freq, tmpDynamicParams, staticParams);
         Leph::VectorLabel outputs = walk.lastOutputs();
         sdkConnection.setMotorAngles(outputs);
         std::this_thread::sleep_for(
                 std::chrono::milliseconds((int)(1000/freq)));
-    }
-}
-
-bool askedExit = false;
-
-void signal_handler(int s)
-{
-    askedExit = true;
-}
-
-int main()
-{
-    struct sigaction sigIntHandler;
-    sigIntHandler.sa_handler = signal_handler;
-    sigemptyset(&sigIntHandler.sa_mask);
-    sigIntHandler.sa_flags = 0;
-    sigaction(SIGINT, &sigIntHandler, NULL);
-
-    Leph::CartWalkProxy walk;
-    Leph::VectorLabel staticParams = walk.buildStaticParams();
-    Leph::VectorLabel dynamicParams = walk.buildDynamicParams();
-    std::cout << (staticParams+dynamicParams) << std::endl;
-
-    Rhoban::MotionCapture motionCapture;
-    motionCapture.setCaptureStream("tcp://192.168.16.10:3232");
-    //motionCapture.averageCoefPos = 0.9;
-    //motionCapture.maxInvalidTick = 20;
-
-    const double forwardReference = -0.336085;
-    const double turnReference = 0.0;
-    const double lateralReference = -0.0763521;
-    const double freq = 50.0;
-    
-    try {
-        std::cout << "Connection..." << std::endl;
-        Leph::SDKConnection sdkConnection;
-        
-        std::cout << "Walk disabled" << std::endl;
-        dynamicParams("enabled") = 0;
-        runWalk(sdkConnection, walk, staticParams, dynamicParams, 2.0);
-        std::cout << "Walk enabled" << std::endl;
-        dynamicParams("enabled") = 1;
-        double deltaStep = 0.0;
-        double deltaLateral = 0.0;
-        double deltaTurn = 0.0;
-        double discountCoef = 0.8;
-        while (!askedExit) {
-            motionCapture.tick(1.0/freq);
-            bool isValid = motionCapture.pos.isValid;
-            double forwardError = motionCapture.pos.z - forwardReference;
-            double turnError = motionCapture.pos.azimuth - turnReference;
-            double lateralError = motionCapture.pos.x - lateralReference;
-
-            if (isValid) {
-                deltaStep = discountCoef*deltaStep + (1.0-discountCoef)*20.0*forwardError;
-                deltaLateral = discountCoef*deltaLateral + (1.0-discountCoef)*125.0*lateralError;
-                deltaTurn = discountCoef*deltaTurn + (1.0-discountCoef)*1.25*turnError;
-            }
-
-            if (isValid) {
-                std::cout << "forward=" << motionCapture.pos.z << " ";
-                std::cout << "lateral=" << motionCapture.pos.azimuth << " ";
-                std::cout << "turn=" << motionCapture.pos.x << std::endl;
-                std::cout << "Deltas: " << deltaStep << " " << deltaLateral << " " << deltaTurn << std::endl;
-            } else {
-                std::cout << "No motion capture" << std::endl;
-            }
-
-            dynamicParams("step") = 12.0 + deltaStep; //Treadmill speed=4
-            dynamicParams("turn") = 0.0 + deltaTurn;
-            dynamicParams("lateral") = 0.0 + deltaLateral;
-
-            if (dynamicParams("step") > 30.0) dynamicParams("step") = 30.0;
-            if (dynamicParams("step") < 0.0) dynamicParams("step") = 0.0;
-            if (dynamicParams("lateral") > 20.0) dynamicParams("lateral") = 20.0;
-            if (dynamicParams("lateral") < -20.0) dynamicParams("lateral") = -20.0;
-            if (dynamicParams("turn") > 50.0) dynamicParams("turn") = 50.0;
-            if (dynamicParams("turn") < -50.0) dynamicParams("turn") = -50.0;
-
-            walk.exec(1.0/freq, dynamicParams, staticParams);
-            Leph::VectorLabel outputs = walk.lastOutputs();
-            sdkConnection.setMotorAngles(outputs);
-            std::this_thread::sleep_for(
-                    std::chrono::milliseconds((int)(1000/freq)));
-        }
-        std::cout << "Walk disabled" << std::endl;
-        dynamicParams("enabled") = 0;
-        runWalk(sdkConnection, walk, staticParams, dynamicParams, 1.0);
-
-        std::cout << "Stopping" << std::endl;
-    } catch (std::string err) {
-        std::cout << "Exception : " << err << std::endl;
     }
 
     return 0;
