ea1bb65c4e948b1e372422463beb652e1de1026a
Leph <leph.quentinrouxel@gmail.com>
2015-02-09 10:19:55 +0100

    Adding MatrixLabel implementation and tests

---
+++ Tests/testMatrixLabel.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <cassert>
+#include "Types/MatrixLabel.hpp"
+
+int main()
+{
+    Leph::MatrixLabel serie;
+    assert(serie.size() == 0);
+    assert(serie.dimension() == 0);
+
+    serie.append(Leph::VectorLabel("x", 1.0, "y", 2.0));
+    serie.append(Leph::VectorLabel("x", 1.0, "y", 3.0));
+    serie.append(Leph::VectorLabel("x", 2.0, "y", -2.0));
+    assert(serie.size() == 3);
+    assert(serie.dimension() == 2);
+    assert(serie[0]("y") == 2.0);
+    
+    assert(serie.range(1, 2).size() == 2);
+    assert(serie.range(1, 2).dimension() == 2);
+    assert(serie.range(1, 2)[0]("y") == 3.0);
+
+    std::cout << serie.mean() << std::endl;
+    std::cout << serie.range(0, 1).mean() << std::endl;
+    std::cout << serie.variance() << std::endl;
+    std::cout << serie.range(0, 1).variance() << std::endl;
+    std::cout << serie.stdDev() << std::endl;
+    std::cout << serie.range(0, 1).stdDev() << std::endl;
+
+    Leph::MatrixLabel serie2 = serie.copy();
+    serie2.normalize();
+    serie.plot().plot("index", "all").render();
+    serie2.plot().plot("index", "all").render();
+
+    serie.save("/tmp/testMatrixLabel.csv");
+    Leph::MatrixLabel serie3;
+    serie3.load("/tmp/testMatrixLabel.csv");
+    assert(serie3.size() == serie.size());
+    serie3.save("/tmp/testMatrixLabel.csv");
+    serie3.plot().plot("index", "all").render();
+
+    return 0;
+}
+
---
+++ Types/MatrixLabel.hpp
@@ -0,0 +1,344 @@
+#ifndef LEPH_MATRIXLABEL_HPP
+#define LEPH_MATRIXLABEL_HPP
+
+#include <vector>
+#include <string>
+#include <memory>
+#include <stdexcept>
+#include <fstream>
+#include "Types/VectorLabel.hpp"
+#include "Plot/Plot.hpp"
+
+namespace Leph {
+
+/**
+ * MatrixLabel
+ *
+ * Represent a time serie 
+ * of VectorLabel
+ */
+class MatrixLabel
+{
+    public:
+
+        /**
+         * Initialization empty
+         */
+        MatrixLabel() :
+            _container(),
+            _indexBegin(0),
+            _indexEnd(-1)
+        {
+            _container = std::make_shared<Container>();
+        }
+
+        /**
+         * Explicitely forbid assignement
+         */
+        MatrixLabel& operator=(const MatrixLabel&) = delete;
+
+        /**
+         * Copy and return a same MatrixLabel instance
+         */
+        inline MatrixLabel copy() const
+        {
+            return MatrixLabel(std::make_shared<Container>(*_container), 
+                _indexBegin, _indexEnd);
+        }
+
+        /**
+         * Write and save given MatrixLabel view into
+         * given file path name
+         */
+        inline void save(const std::string& fileName) const
+        {
+            if (size() == 0) {
+                throw std::logic_error("MatrixLabel view empty");
+            }
+
+            //Open data file
+            std::ofstream logFile(fileName);
+            if (!logFile.is_open()) {
+                throw std::runtime_error(
+                    "MatrixLabel unable to write file: " 
+                    + fileName);
+            }
+
+            //Write first label line
+            logFile << "# ";
+            for (size_t j=0;j<_container->front().size();j++) {
+                logFile << "'" << _container->front().getLabel(j) << "' ";
+            }
+            logFile << std::endl;
+            //Write data
+            for (size_t i=_indexBegin;i<=_indexEnd;i++) {
+                for (size_t j=0;j<_container->front().size();j++) {
+                    logFile << std::setprecision(10) << _container->at(i)(j) << " ";
+                }
+                logFile << std::endl;
+            }
+            
+            logFile.close();
+        }
+
+        /**
+         * Read and append data from given file path name
+         */
+        inline void load(const std::string& fileName)
+        {
+            //Open data file
+            std::ifstream logFile(fileName);
+            if (!logFile.is_open()) {
+                throw std::runtime_error(
+                    "MatrixLabel unable to read file: " 
+                    + fileName);
+            }
+
+            //Read first two data lines
+            Leph::VectorLabel tmp;
+            tmp.readFromCSV(logFile);
+            append(tmp);
+
+            //Read all others
+            while (logFile.good()) {
+                //Skip labels line
+                if (logFile.peek() == '#') {
+                    while (logFile.peek() != '\n') { 
+                        if (!logFile.good()) {
+                            throw std::runtime_error(
+                                "MatrixLabel invalid format (1)");
+                        }
+                        logFile.ignore(); 
+                    }
+                    logFile.ignore(); 
+                } else if (logFile.peek() == EOF) {
+                    break;
+                } 
+                if (!logFile.good()) {
+                    throw std::runtime_error(
+                        "MatrixLabel invalid format (3)");
+                }
+                //Create a new points by copy
+                append(_container->front());
+                //Parse line values
+                //Labels are supposed sorted and all lines
+                //are supposed to have same labels and order 
+                //than first one
+                for (size_t i=0;i<dimension();i++) {
+                    double val;
+                    logFile >> val;
+                    _container->back()(i) = val;
+                    while (logFile.peek() == ' ') {
+                        if (!logFile.good()) {
+                            throw std::runtime_error(
+                                "MatrixLabel invalid format (4)");
+                        }
+                        logFile.ignore();
+                    }
+                    if (!logFile.good()) {
+                        throw std::runtime_error(
+                            "MatrixLabel invalid format (5)");
+                    }
+                }
+                if (logFile.peek() != '\n') {
+                    throw std::runtime_error(
+                        "MatrixLabel invalid format (6)");
+                }
+                logFile.ignore();
+            }
+            logFile.close();
+        }
+
+        /**
+         * Return time serie (view) size and contained 
+         * Vector dimension
+         */
+        inline size_t size() const
+        {
+            if (_container->size() == 0) {
+                return 0;
+            } else {
+                return _indexEnd-_indexBegin+1;
+            }
+        }
+        inline size_t dimension() const
+        {
+            if (size() == 0) {
+                return 0;
+            } else {
+                return _container->front().size();
+            }
+        }
+
+        /**
+         * Return a sliced view of this MatrixLabel
+         */ 
+        inline MatrixLabel range(size_t indexBegin, size_t indexEnd) const
+        {
+            return MatrixLabel(_container, indexBegin, indexEnd);
+        }
+
+        /**
+         * Append given VectorLabel at the end of the serie
+         */
+        inline void append(const VectorLabel& vect)
+        {
+            if (size() != 0 && dimension() != vect.size()) {
+                throw std::logic_error("MatrixLabel invalid dimension");
+            } 
+
+            if (_indexEnd == (size_t)-1 || 
+                _indexEnd == _container->size()-1
+            ) {
+                _indexEnd++;
+            }
+            _container->push_back(vect);
+        }
+
+        /**
+         * Element access
+         */
+        inline const VectorLabel& operator[](size_t index) const
+        {
+            size_t realIndex = index + _indexBegin;
+            if (realIndex < _indexBegin || realIndex > _indexEnd) {
+                throw std::logic_error("MatrixLabel invalid index access");
+            }
+
+            return _container->at(realIndex);
+        }
+        inline VectorLabel& operator[](size_t index)
+        {
+            size_t realIndex = index + _indexBegin;
+            if (realIndex < _indexBegin || realIndex > _indexEnd) {
+                throw std::logic_error("MatrixLabel invalid index access");
+            }
+
+            return _container->at(realIndex);
+        }
+
+        /**
+         * Compute the mean, variance and standard
+         * deviation of this MatrixLabel view
+         */
+        inline VectorLabel mean() const
+        {
+            if (size() == 0) {
+                throw std::logic_error("MatrixLabel empty");
+            }
+
+            VectorLabel sum = _container->front();
+            sum.zeroOp();
+            size_t count = 0;
+            for (size_t i=_indexBegin;i<=_indexEnd;i++) {
+                sum.addOp(_container->at(i));
+                count++;
+            }
+
+            if (count > 0) {
+                sum.divOp(count);
+            }
+            return sum;
+        }
+        inline VectorLabel variance() const
+        {
+            if (size() == 0) {
+                throw std::logic_error("MatrixLabel empty");
+            }
+
+            VectorLabel sum = _container->front();
+            VectorLabel sum2 = _container->front();
+            sum.zeroOp();
+            sum2.zeroOp();
+            size_t count = 0;
+            for (size_t i=_indexBegin;i<=_indexEnd;i++) {
+                sum.addOp(_container->at(i));
+                VectorLabel tmp = _container->at(i);
+                tmp.squareOp();
+                sum2.addOp(tmp);
+                count++;
+            }
+
+            if (count > 0) {
+                sum.divOp(count);
+                sum2.divOp(count);
+            }
+            sum.squareOp();
+            sum2.subOp(sum);
+            return sum2;
+        }
+        inline VectorLabel stdDev() const
+        {
+            VectorLabel var = variance();
+            var.sqrtOp();
+            return var;
+        }
+
+        /**
+         * Normalize the current view
+         */
+        inline void normalize()
+        {
+            VectorLabel m = mean();
+            VectorLabel dev = stdDev();
+            for (size_t i=0;i<dev.size();i++) {
+                if (fabs(dev(i)) < 0.0001) {
+                    dev(i) = 1.0;
+                }
+            }
+
+            for (size_t i=_indexBegin;i<=_indexEnd;i++) {
+                _container->at(i).subOp(m);
+                _container->at(i).divOp(dev);
+            }
+        }
+
+        /**
+         * Return a Plot object with data
+         * of this MatrixLabel view added
+         */
+        inline Plot plot() const
+        {
+            Plot plot;
+            for (size_t i=_indexBegin;i<=_indexEnd;i++) {
+                plot.add(_container->at(i));
+            }
+
+            return plot;
+        }
+
+    private:
+
+        /**
+         * VectorLabel container typedef
+         */
+        typedef std::vector<VectorLabel> Container;
+
+        /**
+         * The vector container
+         */
+        std::shared_ptr<Container> _container;
+
+        /**
+         * Global container index bound
+         * (use to create view)
+         */
+        size_t _indexBegin;
+        size_t _indexEnd;
+
+        /**
+         * Initialization of Matrix view
+         * with existing container and given bounds
+         */
+        MatrixLabel(std::shared_ptr<Container> container, size_t indexBegin, size_t indexEnd) :
+            _container(container),
+            _indexBegin(indexBegin),
+            _indexEnd(indexEnd)
+        {
+        }
+};
+
+}
+
+#endif
+
