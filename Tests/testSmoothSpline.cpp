ac947d89aa6f026d5e25da6db3e3355b987c06e8
Leph <leph.quentinrouxel@gmail.com>
2015-03-12 16:06:44 +0100

    Ajout de l'implémentation et tests des splines minimum jerk

---
+++ Tests/testSmoothSpline.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Utils/SmoothSpline.hpp"
+#include "Plot/Plot.hpp"
+
+int main()
+{
+    Leph::SmoothSpline spline;
+    spline.addPoint(0.0, 0.0, 0.0, 0.0);
+    spline.addPoint(1.0, 1.0, 0.0, 0.0);
+    spline.addPoint(2.0, 0.5, 0.0, 0.0);
+
+    Leph::Plot plot;
+    for (double t=-0.5;t<=2.5;t+=0.01) {
+        plot.add(Leph::VectorLabel(
+            "t", t,
+            "pos", spline.pos(t),
+            "vel", spline.vel(t),
+            "acc", spline.acc(t)
+        ));
+    }
+    plot.plot("t", "all").render();
+
+    return 0;
+}
+
