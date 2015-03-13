48a26da651bbf8a7581b7cc10322f7a56e64afac
Leph <leph.quentinrouxel@gmail.com>
2015-03-13 12:04:14 +0100

    Ajout de l'implémentation et tests de StaticWalk

---
+++ StaticWalk/StaticWalk.hpp
@@ -0,0 +1,94 @@
+#ifndef LEPH_STATICWALK_HPP
+#define LEPH_STATICWALK_HPP
+
+#include "Types/VectorLabel.hpp"
+#include "Model/SigmabanModel.hpp"
+#include "Model/InverseKinematics.hpp"
+
+namespace Leph {
+
+/**
+ * StaticWalk
+ */
+class StaticWalk
+{
+    public:
+
+        /**
+         * Initialization
+         */
+        StaticWalk();
+
+        /**
+         * Build and return initial parameters values
+         */
+        VectorLabel buildParams() const;
+
+        /**
+         * Get and set the walk phase
+         * (between 0 and 1)
+         */
+        double getPhase() const;
+        void setPhase(double phase);
+
+        /**
+         * Return motor output for
+         * walk starting pose using
+         * given parameters
+         */
+        VectorLabel initPose(const VectorLabel& params);
+
+        /**
+         * Update the walk phase, compute and
+         * return the new motors outputs using
+         * given time step and parameters
+         */
+        VectorLabel exec(double dt, const VectorLabel& params);
+
+    private:
+
+        /**
+         * Movement cyclic phase
+         */
+        double _phase;
+
+        /**
+         * Sigmaban model instance
+         */
+        SigmabanModel _model;
+
+        /**
+         * Inverse Kinematics instance 
+         */
+        InverseKinematics _inverseModel;
+        
+        /**
+         * Initial position of COM height 
+         * and foot Y offset in world frame
+         */
+        double _initCOMZOffset;
+        double _initFootYOffset;
+
+        /**
+         * Check Inverse Kinematics error
+         * and throw std::runtime_error if
+         * convergence is failed and if throwError
+         * is true.
+         * Return if convergence is successful
+         */
+        bool checkIKErrors(const InverseKinematics& inv, 
+            bool throwError) const;
+
+        /**
+         * Initialize given Inverse Kinematics
+         * instance
+         * Trunk DOF is enabled if the flag is true
+         */
+        void initIK(InverseKinematics& inv,
+            bool enableTrunkOrientation);
+};
+
+}
+
+#endif
+
