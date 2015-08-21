1a9da16cec3a83cab205391650fce93d7d2fb6b8
Leph <leph.quentinrouxel@gmail.com>
2015-08-21 17:43:56 +0200

    Ajout de l'implémentation et tests de Concept

---
+++ Tests/testConcept.cpp
@@ -0,0 +1,207 @@
+#include <iostream>
+#include <cassert>
+#include "TimeSeries/TimeSeries.hpp"
+#include "TimeSeries/Concept.hpp"
+
+class TestConcept1 : public Leph::Concept
+{
+    public:
+        
+        virtual std::string name() const override
+        {
+            return "TestConcept1";
+        }
+        virtual size_t inputSize() const override
+        {
+            return 2;
+        }
+        virtual size_t outputSize() const override
+        {
+            return 2;
+        }
+        virtual inline size_t parameterSize() const override
+        {
+            return 0;
+        }
+        virtual Leph::MetaParameter defaultParameter(size_t index) const override
+        {
+            (void)index;
+            return Leph::MetaParameter();
+        }
+
+    protected:
+
+        virtual bool doCompute(double time) override
+        {
+            if (
+                !Concept::getInput(0)->isTimeValid(time) ||
+                !Concept::getInput(1)->isTimeValid(time)
+            ) {
+                return false;
+            }
+            
+            Concept::getOutput(0)->append(time, 
+                Concept::getInput(0)->get(time)*2.0);
+            Concept::getOutput(1)->append(time, 
+                Concept::getInput(0)->get(time) + Concept::getInput(1)->get(time));
+
+            return true;
+        }
+};
+
+class TestConcept2 : public Leph::Concept
+{
+    public:
+        
+        virtual std::string name() const override
+        {
+            return "TestConcept2";
+        }
+        virtual size_t inputSize() const override
+        {
+            return 1;
+        }
+        virtual size_t outputSize() const override
+        {
+            return 1;
+        }
+        virtual inline size_t parameterSize() const override
+        {
+            return 0;
+        }
+        virtual Leph::MetaParameter defaultParameter(size_t index) const override
+        {
+            (void)index;
+            return Leph::MetaParameter();
+        }
+
+    protected:
+
+        virtual bool doCompute(double time) override
+        {
+            if (
+                Concept::getInput(0)->isTimeValid(time) &&
+                Concept::getInput(0)->isTimeValid(time-1.0) 
+            ) {
+                Concept::getOutput(0)->append(time, 
+                    Concept::getInput(0)->get(time) 
+                    + Concept::getInput(0)->get(time-1.0));
+                return true;
+            } else {
+                return false;
+            }
+        }
+};
+
+void testCompute()
+{
+    Leph::TimeSeries in("in");
+    Leph::TimeSeries out("out");
+
+    in.append(1.0, 1.0);
+    in.append(2.0, 2.0);
+    in.append(3.0, 3.0);
+    in.append(4.0, 4.0);
+
+    TestConcept2 concept2;
+    concept2.resetParameters();
+    concept2.setInput(0, &in);
+    concept2.setOutput(0, &out);
+
+    assert(concept2.inputSize() == 1);
+    assert(concept2.outputSize() == 1);
+    assert(concept2.name() == "TestConcept2");
+    assert(in.size() == 4);
+    assert(out.size() == 0);
+    
+    assert(concept2.computeAtTime(1.5) == false);
+    assert(in.size() == 4);
+    assert(out.size() == 0);
+    assert(concept2.computeAtTime(2.0) == true);
+    assert(in.size() == 4);
+    assert(out.size() == 1);
+    assert(out[0].time == 2.0);
+    assert(out[0].value == 2.0 + 1.0);
+    
+    assert(concept2.computePropagate() == true);
+    assert(in.size() == 4);
+    assert(out.size() == 3);
+    assert(out[0].time == 4.0);
+    assert(out[0].value == 3.0 + 4.0);
+    assert(out[1].time == 3.0);
+    assert(out[1].value == 2.0 + 3.0);
+    in.save(std::cout);
+    out.save(std::cout);
+    
+    assert(concept2.computePropagate() == false);
+}
+
+int main()
+{
+    Leph::TimeSeries in1("in1");
+    Leph::TimeSeries in2("in2");
+    Leph::TimeSeries out1("out1");
+    Leph::TimeSeries out2("out2");
+
+    in1.append(1.0, 1.0);
+    in1.append(2.0, 2.0);
+    in1.append(3.0, 3.0);
+    in1.append(4.0, 4.0);
+
+    in2.append(1.2, 1.5);
+    in2.append(2.2, 2.5);
+    in2.append(3.2, 3.5);
+    in2.append(4.2, 4.5);
+
+    TestConcept1 concept1;
+    concept1.resetParameters();
+    concept1.setInput(0, &in1);
+    concept1.setInput(1, &in2);
+    concept1.setOutput(0, &out1);
+    concept1.setOutput(1, &out2);
+
+    assert(concept1.inputSize() == 2);
+    assert(concept1.outputSize() == 2);
+    assert(concept1.name() == "TestConcept1");
+    assert(in1.size() == 4);
+    assert(in2.size() == 4);
+    assert(out1.size() == 0);
+    assert(out2.size() == 0);
+        
+    assert(concept1.computeAtTime(1.5) == true);
+    assert(in1.size() == 4);
+    assert(in2.size() == 4);
+    assert(out1.size() == 1);
+    assert(out2.size() == 1);
+    assert(out1[0].time == 1.5);
+    assert(out2[0].time == 1.5);
+    assert(out1[0].value == 3.0);
+    assert(out2[0].value == 3.3);
+    
+    assert(concept1.computePropagate() == true);
+    assert(out1.size() == 6);
+    assert(out2.size() == 6);
+    assert(out1[0].time == 4.0);
+    assert(out1[1].time == 3.2);
+    assert(out1[2].time == 3.0);
+    assert(out1[3].time == 2.2);
+    assert(out2[0].time == 4.0);
+    assert(out2[1].time == 3.2);
+    assert(out2[2].time == 3.0);
+    assert(out2[3].time == 2.2);
+    assert(out1[0].value == 8.0);
+    assert(out1[1].value == 6.4);
+    assert(out1[2].value == 6.0);
+    assert(out1[3].value == 4.4);
+    in1.save(std::cout);
+    in2.save(std::cout);
+    out1.save(std::cout);
+    out2.save(std::cout);
+    
+    assert(concept1.computePropagate() == false);
+
+    testCompute();
+
+    return 0;
+}
+
---
+++ TimeSeries/Concept.hpp
@@ -0,0 +1,309 @@
+#ifndef LEPH_CONCEPT_HPP
+#define LEPH_CONCEPT_HPP
+
+#include <string>
+#include <vector>
+#include <stdexcept>
+#include <limits>
+#include <cmath>
+#include "TimeSeries/TimeSeries.hpp"
+#include "TimeSeries/Optimizable.hpp"
+
+namespace Leph {
+
+/**
+ * Concept
+ *
+ * Abstract base class for
+ * Repressentation linking input to
+ * output series into a graph. 
+ * It could also have optimizable
+ * meta parameters.
+ */
+class Concept : public Optimizable
+{
+    public:
+
+        /**
+         * Initialization
+         */
+        Concept() :
+            Optimizable(),
+            _inputSeries(),
+            _outputSeries()
+        {
+        }
+
+        /**
+         * Empty virtual destructor
+         */
+        virtual ~Concept()
+        {
+        }
+
+        /**
+         * Return Concept name.
+         * (Must be implemented)
+         */
+        virtual std::string name() const = 0;
+
+        /**
+         * Return the number of inputs
+         * and outputs time series 
+         * (Must be implemented)
+         */
+        virtual size_t inputSize() const = 0;
+        virtual size_t outputSize() const = 0;
+
+        /**
+         * Read inputs values, compute and append 
+         * outputs values at given time.
+         * False is returned if input series values are
+         * not available and no output update is performed.
+         */
+        inline bool computeAtTime(double time)
+        {
+            //Check inputs outputs initialization
+            if (_inputSeries.size() == 0) {
+                throw std::logic_error(
+                    "Concept not initialized");
+            }
+
+            //Call real concept implementation
+            return doCompute(time);
+        }
+
+        /**
+         * Find in input series values not
+         * propagated to output series. An
+         * output value is computed for each
+         * new input value.
+         * False is returned if input series values are
+         * not available and no output update is performed.
+         */
+        inline bool computePropagate()
+        {
+            //Float precision for time check
+            double FLOAT_EPSILON = 0.0001;
+
+            //Check inputs outputs initialization
+            if (_inputSeries.size() == 0 || _outputSeries.size() == 0) {
+                throw std::logic_error(
+                    "Concept not initialized");
+            }
+
+            //Check if all input are non empty
+            for (size_t i=0;i<_inputSeries.size();i++) {
+                if (_inputSeries[i]->size() == 0) {
+                    return false;
+                }
+            }
+
+            //Check if there is one non empty output
+            bool isOutputsEmpty = true;
+            for (size_t i=0;i<_outputSeries.size();i++) {
+                if (_outputSeries[i]->size() != 0) {
+                    isOutputsEmpty = false;
+                }
+            }
+            
+            //Look for maximum of input minimum time and minimum
+            //of maximum
+            double inputsTimeMin = _inputSeries.front()->timeMin();
+            double inputsTimeMax = _inputSeries.front()->timeMax();
+            for (size_t i=0;i<_inputSeries.size();i++) {
+                if (_inputSeries[i]->timeMin() > inputsTimeMin) {
+                    inputsTimeMin = _inputSeries[i]->timeMin();
+                }
+                if (_inputSeries[i]->timeMax() < inputsTimeMax) {
+                    inputsTimeMax = _inputSeries[i]->timeMax();
+                }
+            }
+
+            //Find propagate starting time
+            double currentTime;
+            if (isOutputsEmpty) {
+                //If no output already exists, we have to
+                //run propagation from inputs begining
+                currentTime = inputsTimeMin;
+            } else {
+                //Look for maximum output time
+                double outputsTimeMax = _outputSeries.front()->timeMax();
+                for (size_t i=0;i<_outputSeries.size();i++) {
+                    if (_outputSeries[i]->timeMax() > outputsTimeMax) {
+                        outputsTimeMax = _outputSeries[i]->timeMax();
+                    }
+                }
+                if (outputsTimeMax <= inputsTimeMax) {
+                    //We fill values from output
+                    //maximum values if fresh input values
+                    //are available
+                    currentTime = outputsTimeMax;
+                } else {
+                    //Else, nothing to do
+                    return false;
+                }
+            }
+
+            bool isUpdated = false;
+            while (true) {
+                //Find the time associated with the 
+                //first next updated input
+                currentTime += FLOAT_EPSILON;
+                double nextTime = std::numeric_limits<double>::quiet_NaN();
+                bool isFinished = false;
+                for (size_t i=0;i<_inputSeries.size();i++) {
+                    if (currentTime <= _inputSeries[i]->timeMax()) {
+                        size_t indexLow = _inputSeries[i]->getLowerIndex(currentTime);
+                        double timeLow = _inputSeries[i]->at(indexLow).time;
+                        if (std::isnan(nextTime) || timeLow < nextTime) {
+                            nextTime = timeLow;
+                        }
+                    } else {
+                        isFinished = true;
+                    }
+                }
+                //End if one input has reached an end
+                if (isFinished) {
+                    break;
+                }
+                if (std::isnan(nextTime)) {
+                    throw std::logic_error("Concept error nan");
+                }
+                //Try to run concept and update 
+                //output values
+                if (doCompute(nextTime)) {
+                    isUpdated = true;
+                }
+                //Go to next point
+                currentTime = nextTime;
+            }
+
+            return isUpdated;
+        }
+
+        /**
+         * Direct access to internal
+         * input and output time series pointer.
+         */
+        inline const TimeSeries* getInput(size_t index) const
+        {
+            if (_inputSeries.size() == 0) {
+                throw std::logic_error(
+                    "Concept not initialized");
+            }
+            if (index >= inputSize()) {
+                throw std::logic_error("Concept invalid index");
+            }
+            return _inputSeries[index];
+        }
+        inline const TimeSeries* getOutput(size_t index) const
+        {
+            if (_outputSeries.size() == 0) {
+                throw std::logic_error(
+                    "Concept not initialized");
+            }
+            if (index >= outputSize()) {
+                throw std::logic_error("Concept invalid index");
+            }
+            return _outputSeries[index];
+        }
+        inline TimeSeries* getOutput(size_t index)
+        {
+            if (_outputSeries.size() == 0) {
+                throw std::logic_error(
+                    "Concept not initialized");
+            }
+            if (index >= outputSize()) {
+                throw std::logic_error("Concept invalid index");
+            }
+            return _outputSeries[index];
+        }
+
+        /**
+         * Set input and output time series pointer
+         * at given index.
+         * (pointer are intialized to nullptr and
+         * must be defined)
+         */
+        inline void setInput(size_t index, const TimeSeries* ptr)
+        {
+            //Check pointer
+            if (ptr == nullptr) {
+                throw std::logic_error(
+                    "Concept invalid null pointer");
+            }
+            if (index >= inputSize()) {
+                throw std::logic_error("Concept invalid index");
+            }
+            //Initialization if needed
+            checkInit();
+            //Assign
+            _inputSeries[index] = ptr;
+        }
+        inline void setOutput(size_t index, TimeSeries* ptr)
+        {
+            //Check pointer
+            if (ptr == nullptr) {
+                throw std::logic_error(
+                    "Concept invalid null pointer");
+            }
+            if (index >= outputSize()) {
+                throw std::logic_error("Concept invalid index");
+            }
+            //Initialization if needed
+            checkInit();
+            //Assign
+            _outputSeries[index] = ptr;
+        }
+
+    protected:
+
+        /**
+         * Implementation of Concept computation
+         * at given time. Read inputs and generate 
+         * outputs values.
+         * False is returned if input series values are
+         * not available and no output update is performed.
+         * (Must be implemented)
+         */
+        virtual bool doCompute(double time) = 0;
+
+    private:
+
+        /**
+         * Input and output time series container
+         * of size inputSize() and outputSize().
+         */
+        std::vector<const TimeSeries*> _inputSeries;
+        std::vector<TimeSeries*> _outputSeries;
+
+        /**
+         * Initialize input and output series
+         * pointer container if they are still empty
+         */
+        inline void checkInit()
+        {
+            if (_inputSeries.size() == 0 || _outputSeries.size() == 0) {
+                size_t inSize = inputSize();
+                size_t outSize = outputSize();
+                //Check implemetation
+                if (inSize == 0 || outSize == 0) {
+                    throw std::logic_error(
+                        "Concept null input or output size");
+                }
+                //Pointer initialization
+                for (size_t i=0;i<inSize;i++) {
+                    _inputSeries.push_back(nullptr);
+                }
+                for (size_t i=0;i<outSize;i++) {
+                    _outputSeries.push_back(nullptr);
+                }
+            }
+        }
+};
+
+}
+
+#endif
+
