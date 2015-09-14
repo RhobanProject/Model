f33b13fb6d47d5a5f1be9773c663e93ed388a6f9
medrimonia <medrimonia@gmail.com>
2015-09-14 10:33:39 +0900

    First theorical version of ToeWalk available

--- Tests/testToeWalk.cpp
+++ Tests/testToeWalk.cpp
@@ -55,8 +55,8 @@ enum SupportType getSupportType(double t)
   if (internalTime < 0.25)                     return SupportType::LeftBase2LeftToe;
   if (internalTime < 0.25 + simpleSupportTime) return SupportType::LeftToe;
   if (internalTime < 0.5)                      return SupportType::LeftToe2RightBase;
-  if (internalTime < 0.5 + simpleSupportTime)  return SupportType::RightToe;
-  if (internalTime < 0.75)                     return SupportType::RightToe2RightBase;
+  if (internalTime < 0.5 + simpleSupportTime)  return SupportType::RightBase;
+  if (internalTime < 0.75)                     return SupportType::RightBase2RightToe;
   if (internalTime < 0.75 + simpleSupportTime) return SupportType::RightToe;
   if (internalTime < 1)                        return SupportType::RightToe2LeftBase;
   throw std::logic_error("Invalid internal time in getSupportType");
@@ -72,9 +72,9 @@ int main()
   Leph::ModelViewer viewer(1200, 900);
 
   double t = 0;
-  cycleLength = 200;
+  cycleLength = 400;
   doubleSupportRatio = 0.5;//Ratio of time spent in double support
-  simpleSupportRatio = 0.5;//Ratio of time spent in simple support
+  simpleSupportRatio = 1 - doubleSupportRatio;//Ratio of time spent in simple support
 
   // Unit is cycle
   simpleSupportTime = simpleSupportRatio / 4;
@@ -85,21 +85,27 @@ int main()
   double trunkZ = 0.45;
   double maxToe = -40 * M_PI / 180;
   double maxFFZ = 0.05;
-  double stepLength = 0.3;
+  double stepLength = 0.2;
 
   double startLeftX = 0;
   double startRightX = 0;
+  double startLeftY = 0;
+  double startRightY = 0;
   double startTrunkX = 0;
+  double startTrunkY = 0;
 
   double footBaseToToe = 0.08;//TODO calculate properly
   
   Leph::Chrono chrono;
 
   std::cerr << "time"
+            << ",phaseTime"
             << ",targetTrunkX"
+            << ",targetTrunkY"
             << ",targetLeftX"
             << ",targetRightX"
             << ",realTrunkX"
+            << ",realTrunkY"
             << ",realLeftX"
             << ",realRightX"
             << std::endl;
@@ -107,7 +113,7 @@ int main()
   while (viewer.update()) {
     double internalTime = getInternalTime(t);
 
-    // 1. Update pressure TODO improve
+    // 1. Update pressure TODO improve by using continuous data. Maybe add noise
     model.setPressure("LeftBase" , 0, 0, 0);
     model.setPressure("LeftToe"  , 0, 0, 0);
     model.setPressure("RightBase", 0, 0, 0);
@@ -127,67 +133,132 @@ int main()
 
     // 2. Update base and update start position if necessary
     model.updateBase();
-    enum SupportType support = getSupportPhase(t);
-    if (support != getSupportPhase(t-1)) {// Switched phase from last time
+    enum SupportType support = getSupportType(t);
+    if (support != getSupportType(t-1)) {// Switched phase from last time
       startLeftX  = model.get().position("left_toe_tip" ,"origin").x();
+      startLeftY  = model.get().position("left_toe_tip" ,"origin").y();
       startRightX = model.get().position("right_toe_tip","origin").x();
+      startRightY = model.get().position("right_toe_tip","origin").y();
       startTrunkX = model.get().position("trunk","origin").x();
+      startTrunkY = model.get().position("trunk","origin").y();
     }
 
     double endLeftX = startLeftX;
+    double endLeftY = startLeftY;
     double endRightX = startRightX;
+    double endRightY = startRightY;
     double endTrunkX = 0;
+    double endTrunkY = 0;
     double leftZ = 0;
     double rightZ = 0;
-    double trunkY = 0;
 
 
 
-    double phaseTime = 0; //Internal phase ratio
-    // Simple Support: Left Base
-    if (internalTime < simpleSupportTime){
-      phaseTime = (simpleSupportTime - internalTime) / simpleSupportTime;
-      model.get().setDOF("right_toe", phaseTime * maxToe);
+    double phaseTime = getPhaseTime(t);
+    switch(support) {
+    case LeftBase: {
+      model.get().setDOF("left_toe", 0);
+      model.get().setDOF("right_toe", (1 - phaseTime) * maxToe);
+      leftZ = 0;
       rightZ = phaseTime * maxFFZ;
-      endRightX = startLeftX + stepLength * simpleSupportTime / flyingFootTime;
+      double diffX = (startLeftX + stepLength - startRightX);
+      double diffY = (startLeftY - feetSpacing - startRightY);
+      endRightX = startRightX + diffX * simpleSupportTime / flyingFootTime;
+      endRightY = startRightY + diffY * simpleSupportTime / flyingFootTime;
       endTrunkX = startLeftX - footBaseToToe;
-      trunkY = model.get().position("left_toe_tip" ,"origin").y();
+      endTrunkY = startLeftY;
+      break;
     }
-    // Double Support: Left Base -> Left Toe 
-    if (internalTime < 0.25){
-      phaseTime = (simpleSupportTime - internalTime) / simpleSupportTime;
+    case LeftBase2LeftToe: {
+      model.get().setDOF("left_toe", 0);
+      model.get().setDOF("right_toe", 0);
+      leftZ = 0;
       rightZ = maxFFZ;
-      endRightX = startLeftX + stepLength * (simpleSupportTime + doubleSupportTime) / flyingFootTime;
-      endTrunkX = startLeftX - footBaseToToe;
-      trunkY = model.get().position("left_toe_tip" ,"origin").y();
+      double remainingFFTime = flyingFootTime - simpleSupportTime;
+      double diffX = (startLeftX + stepLength - startRightX);
+      double diffY = (startLeftY - feetSpacing - startRightY);
+      endRightX = startRightX + diffX * doubleSupportTime / remainingFFTime;
+      endRightY = startRightY + diffY * doubleSupportTime / remainingFFTime;
+      endTrunkX = startLeftX;
+      endTrunkY = startLeftY;
+      break;
     }
-    else if (internalTime < 0.25 + 0.25 * simpleSupportRatio){
-      model.get().setDOF("left_toe", (internalTime - 0.25) * 4 * maxToe);
-      rightZ = 4 * (0.5 - internalTime) * maxFFZ;
+    case LeftToe: {
+      model.get().setDOF("left_toe", phaseTime * maxToe);
+      model.get().setDOF("right_toe", 0);
+      leftZ = 0;
+      rightZ = (1 - phaseTime) * maxFFZ;
       endRightX = startLeftX + stepLength;
-      endTrunkX = startLeftX + stepLength / 2;
-      trunkY = model.get().position("left_toe_tip" ,"origin").y();
+      endRightY = startLeftY - feetSpacing;
+      endTrunkX = startLeftX;
+      endTrunkY = startLeftY;
+      break;
     }
-    else if (internalTime < 0.75){
-      model.get().setDOF("left_toe", (0.75 - internalTime) * 4 * maxToe);
-      leftZ = 4 * (internalTime - 0.5) * maxFFZ;
-      endLeftX = startRightX + stepLength;
-      endTrunkX = startRightX + stepLength / 2;
-      trunkY = model.get().position("right_toe_tip" ,"origin").y();
+    case LeftToe2RightBase: {
+      model.get().setDOF("left_toe", maxToe);
+      model.get().setDOF("right_toe", 0);
+      leftZ = 0;
+      rightZ = 0;
+      endTrunkX = startRightX - footBaseToToe;
+      endTrunkY = startRightY;
+      break;
     }
-    else{
-      model.get().setDOF("right_toe", (internalTime - 0.75) * 4 * maxToe);
-      leftZ = 4 * (1.0 - internalTime) * maxFFZ;
+    case RightBase: {
+      model.get().setDOF("right_toe", 0);
+      model.get().setDOF("left_toe", (1 - phaseTime) * maxToe);
+      rightZ = 0;
+      leftZ = phaseTime * maxFFZ;
+      endRightX = startRightX;
+      double diffX = (startRightX + stepLength - startLeftX);
+      double diffY = (startRightY + feetSpacing - startLeftY);
+      endLeftX = startLeftX + diffX * simpleSupportTime / flyingFootTime;
+      endLeftY = startLeftY + diffY * simpleSupportTime / flyingFootTime;
+      endTrunkX = startRightX - footBaseToToe;
+      endTrunkY = startRightY;
+      break;
+    }
+    case RightBase2RightToe: {
+      model.get().setDOF("right_toe", 0);
+      model.get().setDOF("left_toe", 0);
+      rightZ = 0;
+      leftZ = maxFFZ;
+      double remainingFFTime = flyingFootTime - simpleSupportTime;
+      double diffX = (startRightX + stepLength - startLeftX);
+      double diffY = (startRightY + feetSpacing - startLeftY);
+      endLeftX = startLeftX + diffX * doubleSupportTime / remainingFFTime;
+      endLeftY = startLeftY + diffY * doubleSupportTime / remainingFFTime;
+      endTrunkX = startRightX;
+      endTrunkY = startRightY;
+      break;
+    }
+    case RightToe: {
+      model.get().setDOF("right_toe", phaseTime * maxToe);
+      model.get().setDOF("left_toe" , 0);
+      rightZ = 0;
+      leftZ = (1 - phaseTime) * maxFFZ;
       endLeftX = startRightX + stepLength;
-      endTrunkX = startRightX + stepLength / 2;
-      trunkY = model.get().position("right_toe_tip" ,"origin").y();
+      endLeftY = startRightY + feetSpacing;
+      endTrunkX = startRightX;
+      endTrunkY = startRightY;
+      break;
+    }
+    case RightToe2LeftBase: {
+      model.get().setDOF("right_toe", maxToe);
+      model.get().setDOF("left_toe", 0);
+      rightZ = 0;
+      leftZ = 0;
+      endTrunkX = startLeftX - footBaseToToe;
+      endTrunkY = startLeftY;
+      break;
+    }
     }
 
-    double stepTime = fmod(internalTime, 0.5) * 2;
-    //if (stepTime == 0) stepTime = 1;
-    double leftX  = stepTime * endLeftX  + (1 - stepTime) * startLeftX;
-    double rightX = stepTime * endRightX + (1 - stepTime) * startRightX;
-    double trunkX = stepTime * endTrunkX + (1 - stepTime) * startTrunkX;
+    double leftX  = phaseTime * endLeftX  + (1 - phaseTime) * startLeftX;
+    double leftY  = phaseTime * endLeftY  + (1 - phaseTime) * startLeftY;
+    double rightX = phaseTime * endRightX + (1 - phaseTime) * startRightX;
+    double rightY = phaseTime * endRightY + (1 - phaseTime) * startRightY;
+    double trunkX = phaseTime * endTrunkX + (1 - phaseTime) * startTrunkX;
+    double trunkY = phaseTime * endTrunkY + (1 - phaseTime) * startTrunkY;
 
 
     Leph::InverseKinematics inv(model.get());
@@ -218,8 +289,8 @@ int main()
     inv.addTargetOrientation("RightFoot", "right_toe_tip");
 
     //Set Positions
-    inv.targetPosition("LeftFoot")  = Eigen::Vector3d(leftX      ,  feetSpacing/2,      leftZ);
-    inv.targetPosition("RightFoot") = Eigen::Vector3d(rightX     , -feetSpacing/2,      rightZ);
+    inv.targetPosition("LeftFoot")  = Eigen::Vector3d(leftX , leftY,  leftZ );
+    inv.targetPosition("RightFoot") = Eigen::Vector3d(rightX, rightY, rightZ);
     inv.targetPosition("trunk")     = Eigen::Vector3d(trunkX, trunkY, trunkZ);
 
     //Set Orientations
@@ -228,10 +299,13 @@ int main()
     inv.targetOrientation("trunk")     = Eigen::Matrix3d::Identity();
 
     std::cerr << t
+              << "," << phaseTime
               << "," << trunkX
+              << "," << trunkY
               << "," << leftX
               << "," << rightX
               << "," << model.get().position("trunk"    , "origin").x()
+              << "," << model.get().position("trunk"    , "origin").y()
               << "," << model.get().position("left_toe" , "origin").x()
               << "," << model.get().position("right_toe", "origin").x()
               << std::endl;
@@ -249,10 +323,6 @@ int main()
     viewer.addTrackedPoint(pt); 
 
 
-    //double angle = (sin(t / period) * amplitude + amplitude) / 2;
-    //model.setDOF("left_hip_pitch", - angle);
-    //model.setDOF("left_knee", 2 * angle);
-    //model.setDOF("left_ankle_pitch", - angle);
     Leph::ModelDraw(model.get(), viewer);
     t += 1;
   }
