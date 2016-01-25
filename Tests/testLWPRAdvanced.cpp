5d039adbf73222db3e2f143b9b419c1dab301aeb
Leph <leph.quentinrouxel@gmail.com>
2016-01-25 17:55:07 +0100

    Commiting old temporary testLWPRAdvanced

--- Tests/testLWPRAdvanced.cpp
+++ Tests/testLWPRAdvanced.cpp
@@ -41,6 +41,15 @@ double nonConvexFunction(double t, int mode)
     }
 }
 
+double goalFunction(double t)
+{
+    return sin(2.0*3.14*t) + 0.4*sin(3.0*3.14*t);
+}
+double readFunction(double t)
+{
+    return 0.8*goalFunction(t-0.1) + 0.5 + 0.6*sin(3.14*2.0*t);
+}
+
 /**
  * Try to learn non convex function
  */
@@ -109,8 +118,81 @@ void testNonConvex()
         Leph::Plot::Points).render();
 }
 
+/**
+ * Try to optimize and find lag
+ */
+double testLag(double lag, double delta)
+{
+    size_t inputDim = 2;
+    Eigen::VectorXd params = Leph::LWPRInitParameters(inputDim);
+    std::vector<Eigen::VectorXd> trainInputs;
+    std::vector<double> trainOutputs;
+    
+    Leph::Plot plot;
+    for (double t=0;t<40.0;t+=0.02) {
+        double goal = goalFunction(t);
+        double read = readFunction(t) + 0.3*dRand();
+        plot.add(Leph::VectorLabel(
+            "t", t, 
+            "goal", goal,
+            "read", read
+        ));
+        
+        Eigen::VectorXd x(inputDim);
+        x << goalFunction(t-lag), goalFunction(t-lag-delta);
+        trainInputs.push_back(x);
+        trainOutputs.push_back(read);
+    }
+    plot.plot("t", "all").render();
+    
+    params = Leph::LWPROptimizeParameters(inputDim,
+        params, trainInputs, trainOutputs, trainInputs, trainOutputs,
+        100, true);
+
+    LWPR_Object model = Leph::LWPRInit(inputDim, params);
+    for (size_t i=0;i<trainInputs.size();i++) {
+        Eigen::VectorXd y(1);
+        y << trainOutputs[i];
+        model.update(trainInputs[i], y);
+    }
+    
+    plot.clear();
+    double mse = 0.0;
+    for (double t=0;t<=6.0;t+=0.02) {
+        Eigen::VectorXd x(inputDim);
+        x << goalFunction(t-lag), goalFunction(t-lag-delta);
+        Eigen::VectorXd yp = model.predict(x);
+        plot.add(Leph::VectorLabel(
+            "t", t, 
+            "x0", goalFunction(t-lag),
+            "x1", goalFunction(t-lag-delta),
+            "read", readFunction(t),
+            "fitted", yp(0)
+        ));
+        mse += pow(yp(0) - readFunction(t), 2);
+    }
+    plot
+        .plot("t", "all")
+        .render();
+    plot
+        .plot("x0", "x1", "fitted", Leph::Plot::Points)
+        .plot("x0", "x1", "read", Leph::Plot::Points)
+        .render();
+
+    return mse;
+}
+
 int main()
 {
+    /*
+    for (double d=0.0;d<0.3;d+=0.02) {
+        for (double l=0.0;l<0.3;l+=0.02) {
+            std::cout << l << " " << d << " " << testLag(l, d) << std::endl;
+        }
+    }
+    */
+    testLag(0.12, 0.12);
+    return 0;
     testNonConvex();
     return 0;
 }
