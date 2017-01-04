dfefe428b8e0e23904532c6465398fd687995610
Leph <leph.quentinrouxel@gmail.com>
2017-01-04 18:21:10 +0100

    Test TrajectoryParameters

--- Tests/testTrajectoryParameters.cpp
+++ Tests/testTrajectoryParameters.cpp
@@ -11,10 +11,15 @@ int main()
     params.add("test_x", 1.0, true);
     params.add("test_y", 2.0, true);
     params.add("test_z", 3.0, true);
+    params.cpy("alias", "test_z");
     params.print();
     Eigen::VectorXd vect = params.buildVector();
     std::cout << vect.transpose() << std::endl;
     std::cout << params.getVect("test", vect).transpose() << std::endl;
+    std::cout << params.get("alias", vect) << std::endl;
+    params.set("alias") = 4.0;
+    std::cout << params.get("alias") << std::endl;
+    std::cout << params.get("test_z") << std::endl;
     vect.setZero();
     std::cout << params.get("test1", vect) << std::endl;
     std::cout << params.get("test3", vect) << std::endl;
