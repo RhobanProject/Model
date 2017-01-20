3891c8d62ceb14581f8e23df942dd14a24e84771
Leph <leph.quentinrouxel@gmail.com>
2017-01-20 21:47:59 +0100

    Ajout de testHumanoidSimulation

--- Tests/testHumanoidSimulation.cpp
+++ Tests/testHumanoidSimulation.cpp
@@ -4,49 +4,117 @@
 #include "Viewer/ModelDraw.hpp"
 #include "IKWalk/IKWalk.hpp"
 #include "Model/HumanoidFloatingModel.hpp"
+#include "Model/HumanoidFixedModel.hpp"
+#include "Utils/AxisAngle.h"
+#include "Plot/Plot.hpp"
+#include "Model/NamesModel.h"
 
 /**
- * DOF names
+ * Test Humanoid foot contacts
  */
-static std::vector<std::string> dofsNames = {
-    "head_pitch", "head_yaw",
-    "left_shoulder_pitch", "left_shoulder_roll", "left_elbow",
-    "left_hip_yaw", "left_hip_pitch", "left_hip_roll",
-    "left_knee", "left_ankle_pitch", "left_ankle_roll",
-    "right_shoulder_pitch", "right_shoulder_roll", "right_elbow",
-    "right_hip_yaw", "right_hip_pitch", "right_hip_roll",
-    "right_knee", "right_ankle_pitch", "right_ankle_roll",
-};
-
-int main()
+void testContacts()
 {
     Leph::HumanoidSimulation sim(Leph::SigmabanModel);
     Leph::ModelViewer viewer(1200, 900);
+    Leph::HumanoidFixedModel goalModel(Leph::SigmabanModel);
+    
+    Eigen::Vector3d trunkPos(-0.00557785331559037,  -0.0115849568418458, 0.28);
+    //Eigen::Vector3d trunkAxis(-0.672036398746933, 0.0743358280850477, 0.0028323027017884);
+    Eigen::Vector3d trunkAxis(0.0, 0.0, 0.0);
+    Eigen::Vector3d footPos(0.0, -0.12, 0.02);
+    //Eigen::Vector3d footPos(0.0208647084129351, -0.095, 0.0591693358237435);
+    //trunkPos.y() += 0.04;
+    
+    bool success = goalModel.trunkFootIK(
+        Leph::HumanoidFixedModel::LeftSupportFoot,
+        trunkPos,
+        Leph::AxisToMatrix(trunkAxis),
+        footPos,
+        Leph::AxisToMatrix(Eigen::Vector3d::Zero()));
+    if (!success) {
+        std::cout << "IK ERROR" << std::endl;
+        exit(1);
+    }
+    for (const std::string& name : Leph::NamesDOF) {
+        sim.setPos(name, goalModel.get().getDOF(name));
+        sim.setGoal(name, goalModel.get().getDOF(name));
+    }
+    
+    sim.putOnGround();
+    sim.putFootAt(0.0, 0.0);
+
+    Leph::Plot plot;
+    double t = 0.0;
+    size_t count = 0;
+    while (viewer.update()) {
+        count++;
+        sim.setGoal("left_ankle_roll", goalModel.get().getDOF("left_ankle_roll")-0.4*sin(2.0*3.14*t*0.05));
+        std::cout << "********** " << count << " *********" << std::endl;
+        sim.update(0.001);
+        t += 0.001;
+        sim.printCleatsStatus();
+        sim.printCleatsStatus(plot);
+        Leph::ModelDraw(sim.model(), viewer);
+        Leph::CleatsDraw(sim, viewer);
+        Eigen::Vector3d com = sim.model().centerOfMass("origin");
+        com.z() = 0.0;
+        viewer.addTrackedPoint(
+            com, Leph::ModelViewer::Red);
+        //if (count >= 450) break;
+        //if (count >= 208) break;
+        //if (count >= 3810) break;
+    }
+    plot
+        .plot("index", "left_cleat_1:force")
+        .plot("index", "left_cleat_2:force")
+        .plot("index", "left_cleat_3:force")
+        .plot("index", "left_cleat_4:force")
+        .plot("index", "left_cleat_1:isActive")
+        .plot("index", "left_cleat_2:isActive")
+        .plot("index", "left_cleat_3:isActive")
+        .plot("index", "left_cleat_4:isActive")
+        .render();
+    plot
+        .plot("index", "right_cleat_1:force")
+        .plot("index", "right_cleat_2:force")
+        .plot("index", "right_cleat_3:force")
+        .plot("index", "right_cleat_4:force")
+        .render();
+}
+
+/**
+ * Test IKWalk simulation
+ */
+void testWalk()
+{
+    Leph::HumanoidSimulation sim(Leph::SigmabanModel);
     Leph::HumanoidFloatingModel goalModel(Leph::SigmabanModel);
+    Leph::ModelViewer viewer(1200, 900);
     
     //Initialing IKWalk
     Leph::IKWalk::Parameters params;
-    params.freq = 1.5;
+    //Mowgly working 2016 parameters
+    params.freq = 1.7;
     params.enabledGain = 1.0;
-    params.supportPhaseRatio = 0.6;
-    params.footYOffset = 0.02;
-    params.stepGain = 0.05;
-    params.riseGain = 0.06;
+    params.supportPhaseRatio = 0.0;
+    params.footYOffset = 0.025;
+    params.stepGain = 0.0;
+    params.riseGain = 0.035;
     params.turnGain = 0.0;
     params.lateralGain = 0.0;
-    params.trunkZOffset = 0.05;
-    params.swingGain = 0.02;
+    params.trunkZOffset = 0.02;
+    params.swingGain = 0.01999999955;
     params.swingRollGain = 0.0;
-    params.swingPhase = 0.0;
-    params.stepUpVel = 2.0;
-    params.stepDownVel = 2.0;
-    params.riseUpVel = 2.0;
-    params.riseDownVel = 2.0;
-    params.swingPause = 0.1;
-    params.swingVel = 5.0;
-    params.trunkXOffset = 0.0;
+    params.swingPhase = 0.25;
+    params.stepUpVel = 5.0;
+    params.stepDownVel = 5.0;
+    params.riseUpVel = 5.0;
+    params.riseDownVel = 5.0;
+    params.swingPause = 0.0;
+    params.swingVel = 4.0;
+    params.trunkXOffset = 0.009999999775;
     params.trunkYOffset = 0.0;
-    params.trunkPitch = 0.25;
+    params.trunkPitch = 0.1919862181;
     params.trunkRoll = 0.0;
     params.extraLeftX = 0.0;
     params.extraLeftY = 0.0;
@@ -67,24 +135,32 @@ int main()
         //Run walk generator
         Leph::IKWalk::walk(goalModel, params, phase, 0.01);
         //Import target position
-        for (const std::string& name : dofsNames) {
+        for (const std::string& name : Leph::NamesDOF) {
             sim.setGoal(name, goalModel.getDOF(name));
         }
+        //On first iteration, assign pos/vel state
         if (isFirstIteration) {
-            for (const std::string& name : dofsNames) {
+            for (const std::string& name : Leph::NamesDOF) {
                 sim.setPos(name, goalModel.getDOF(name));
+                sim.setVel(name, 0.0);
             }
             sim.putOnGround();
             isFirstIteration = false;
         }
-
+        //Run simulation
         for (int k=0;k<100;k++) {
             sim.update(0.0001);
         }
+        //Display
         Leph::ModelDraw(sim.model(), viewer);
         Leph::CleatsDraw(sim, viewer);
-        std::cout << sim.getWeightSum() << " " << sim.getWeightLeftRatio() << std::endl;
     }
+}
+
+int main()
+{
+    testContacts();
+    testWalk();
 
     return 0;
 }
